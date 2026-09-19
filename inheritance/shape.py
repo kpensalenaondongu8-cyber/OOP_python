2fa8ea2b0994dd117dfd9e673b93be8e242b06b1
kpensalenaondongu8-cyber <kpensalenaondongu8@gmail.com>
2026-09-19 14:21:22 +0100

    Shape inheritance oop

--- inheritance/shape.py
+++ inheritance/shape.py
@@ -1,23 +1,23 @@
 class Shape:
     def __init__(self, name):
-     self.name = name
+        self.name = name
 
-    def area(self):    
-        return 0
 class Circle(Shape):
-    def __init__ (self, radius):       
-       
+    def __init__(self, name, radius):
+        super().__init__(name)
         self.radius = radius
     def area(self):
-        return 3.14 * self.radius ** 2
-       
+        return 3.14 * self.radius * 2
+
 class Square(Shape):
-    def area(self, side):
-           return side **2
-    
-all_in_one = Shape("Circle")  
-dog = Circle(10)
-x = Square(20)
-print(all_in_one.area())
-print(dog.area())
-print(x.area(2))  
+    def __init__(self, name, paramter):
+        super().__init__(name) 
+        self.paramter = paramter
+    def area(self):
+        return self.paramter ** 2
+
+all_in = Circle("circle", 4)
+all_o = Square("square", 4)
+print(all_in.area())
+print(all_o.area())                       
+        
